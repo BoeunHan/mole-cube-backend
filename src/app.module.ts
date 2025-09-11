f6403d9216fbe31a10419ed5a545e35244fd92e3
BoeunHan <h.boeunn@gmail.com>
2025-09-12 01:07:15 +0900

    feat: 웹소켓 gateway 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GameGateway } from './gateway/game.gateway';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
+  providers: [GameGateway],
 })
 export class AppModule {}
---
+++ src/gateway/game.gateway.ts
@@ -0,0 +1,38 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: process.env.CLIENT_URL,
+  },
+})
+export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  private players: Record<string, string> = {};
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+    delete this.players[client.id];
+
+    this.server.emit('playersUpdate', this.players);
+  }
+
+  @SubscribeMessage('setNickname')
+  handleSetNickname(client: Socket, nickname: string) {
+    this.players[client.id] = nickname;
+
+    this.server.emit('playersUpdate', this.players);
+  }
+}
